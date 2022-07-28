fa1736d643ae304b559120ea17b98536a058ac2e
Chioma <chibabebest01@gmail.com>
2022-07-28 19:14:34 +0100

    changed settings database config

--- Auxano_project/settings.py
+++ Auxano_project/settings.py
@@ -163,8 +163,11 @@ django_heroku.settings(locals())
 
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-print(DATABASES)
+DATABASES['default'].update({'conn_max_age':600, 'ssl_require': True})
+dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
+# print(DATABASES)
 # DATABASES['default'] += dj_database_url.config(conn_max_age=600, ssl_require=True)
