fdd4b2e5557f409306f40cd88ccc05df990ec000
Chioma <chibabebest01@gmail.com>
2022-07-28 19:00:17 +0100

    added print(databases) to settings.py

--- Auxano_project/settings.py
+++ Auxano_project/settings.py
@@ -166,4 +166,5 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
+print(DATABASES)
 # DATABASES['default'] += dj_database_url.config(conn_max_age=600, ssl_require=True)
